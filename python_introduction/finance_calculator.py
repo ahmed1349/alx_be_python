9f168b3cf02b76c8f7d75c7a3094ca51b3391542
ahmed <brillianta134@gmail.com>
2024-09-11 16:26:18 +0300

    the changeds is done

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,5 +2,5 @@ monthlyincome=int(input("Enter your monthly income:"))
 monthlyexpenses=int(input("Enter your total monthly expenses:"))
 monthlysavings = monthlyincome - monthlyexpenses
 ProjectedSavings = monthlysavings * 12 + (monthlysavings * 12 * 0.05)
-print("Your monthly savings are $",monthlysavings)
-print("Projected savings after one year, with interest, is: $",ProjectedSavings)
+print("Your monthly savings are ",monthlysavings)
+print("Projected savings after one year, with interest, is: ",ProjectedSavings)
